0d20c18ee1cd904ca0dea6e8e222308984142f26
rideau <93993119+adderallbaby@users.noreply.github.com>
2022-07-04 17:10:59 +0300

    Update VisMealy.java

--- module3/VisMealy.java
+++ module3/VisMealy.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 public class VisMealy {
-    public static void outPut(char[][] exit, int[][] crossover, int start){
+    public static void outPut(String[][] exit, int[][] crossover, int start){
         System.out.println("digraph {");
-        System.out.println("\trankdir = LR");
+        System.out.println("    rankdir = LR");
         for (int i = 0; i < crossover.length; i++) {
             for (int j = 0; j < crossover[i].length; j++) {
-                if (crossover[i][j] != 0) {
-                    System.out.print("\t" +i + "->" + +crossover[i][j] + "[label = \"" + (char) (j + 97) + "(" + (exit[i][j]) + ")" + "\"]\n");
+                if (!(exit[i][j]==null)){
+                    System.out.print("    "+i + " -> " + +crossover[i][j] + "[label = \"" + (char) (j + 97) + "(" + (exit[i][j]) + ")" + "\"]\n");
                 }
             }
         }
@@ -15,14 +15,14 @@ public class VisMealy {
     }
     public static void main(String[] args) {
         int n,m,start,cr;
-        char sym;
+        String sym;
         Scanner scanner = new Scanner(System.in);
 
         n = scanner.nextInt();
         m = scanner.nextInt();
         start = scanner.nextInt();
-        int[][] crossover = new int[n][n];
-        char[][] exit = new char[n][n];
+        int[][] crossover = new int[2*n][m];
+        String[][] exit = new String[2*n][m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 crossover[i][j] =scanner.nextInt();
@@ -30,7 +30,7 @@ public class VisMealy {
         }
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                exit[i][j] =scanner.next().charAt(0);
+                exit[i][j] =scanner.next();
 
             }
         }
